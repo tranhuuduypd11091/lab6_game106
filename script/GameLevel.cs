cac96fdb1cb7c9bdf538c73aa2d1e7c6554f6cb3
tranhuuduypd11091 <12345duytran@gmail.com>
2024-12-03 15:22:23 +0700

    lab6

---
+++ script/GameLevel.cs
@@ -0,0 +1,63 @@
+using ModelGameLevel;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameLevel : MonoBehaviour
+{
+    public GameObject PrefabGameLevel;
+    public RectTransform ParentGameLevel;
+
+    private IEnumerator GetRequestAPIGameLevel()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("https://localhost:7039/api/APIGame/GetAllGameLevel"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError ||
+                www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("API call failed: " + www.error);
+            }
+            else
+            {
+                try
+                {
+                    ResponseAPI response = JsonConvert.DeserializeObject<ResponseAPI>(www.downloadHandler.text);
+                    HandleGetResponseLevel(response);
+                }
+                catch (System.Exception ex)
+                {
+                    Debug.LogError("Exception during JSON parsing: " + ex.Message);
+                }
+            }
+        }
+    }
+    public void HandleGetResponseLevel(ResponseAPI response)
+    {
+        if (response != null && response.isSuccess)
+        {
+            if (response.data != null)
+            {
+                foreach (var level in response.data)
+                {
+                    GameObject game = Instantiate(PrefabGameLevel, ParentGameLevel);
+                    game.GetComponent<LevelGameData>().levelId = level.levelId;
+                    game.GetComponent<LevelGameData>().title = level.title;
+                    game.GetComponent<LevelGameData>().description = level.description;
+                    game.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = level.title;
+                }
+            }
+        }
+    }
+    private void Start()
+    {
+        StartCoroutine(GetRequestAPIGameLevel());
+    }
+
+
+}
+
